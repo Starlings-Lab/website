63e8582b0169ebbf7af82fdcaf5d0d114aa629ae
Nitin Bhardwaj <nitin.bhardwaj@biddano.com>
2022-12-08 10:43:28 +0530

    Website v1.0

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# website
+# Starlings Lab Website
+
+## Tech Stack
+
+Parcel, Jade/PUG, SCSS, Barbourn, TypeScript
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/starlings-lab/website.git
+```
+
+Go to the project directory
+
+```bash
+  cd website
+```
+
+Install dependencies
+
+```bash
+  yarn
+```
+
+Start the server
+
+```bash
+  yarn dev
+```
+
+## Deployment
+
+To build & deploy this project run:
+
+```bash
+  yarn build
+```
+Then target below mentioned directory and deploy this directory to your hosting
+
+```bash
+  ./dist
+```
+
+
+## Authors
+
+- [@taisukemino](https://github.com/taisukemino)
+- [@nitinnnn](https://github.com/nitinnnn)
---
+++ src/assets/constants/GOOGLE_THEME.js
@@ -0,0 +1,84 @@
+export const THEME = [
+  {
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8372a7"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#564776"
+      },
+      {
+        "weight": 0.5
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#725ba4"
+      },
+      {
+        "weight": 1.5
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+];
